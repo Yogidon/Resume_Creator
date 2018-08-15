f9e47067e207164b14a6a58ef35cf18be75859a7
yogi.yogesh361 <yogi.yogesh361@gmail.com>
2018-08-15 20:33:21 +0530

    Mongoose Used.

--- server.js
+++ server.js
@@ -5,16 +5,45 @@ const PORT = process.env.PORT || 3000;
 const mongodb = require('mongodb');
 const bodyParser = require('body-parser');
 const dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017');
+const mongoose = require('mongoose');
 
 hbs.registerPartials('./views/partials');
 app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/',express.static('./public'));
 
+const courseSchema = new mongoose.Schema({
+    name : String,
+    course : String,
+    dob : { type: Date, default: Date.now},
+    age : Number
+});
+
+const Course = mongoose.model('course', courseSchema);
+
+async function createCourse(name, courses, dob,age ){
+    const course = new Course({
+        name : name ,
+        course:courses,
+        dob:dob,
+        age:age
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+mongoose.connect('mongodb://localhost/admin')
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch(err => console.error('Could not connect to mongodb ...', err));
+
 app.post('/post-feedback', function (req, res) {
-    dbConn.then(function(db) {
-        delete req.body._id;
-        db.collection('feedbacks').insertOne(req.body);
+    dbConn.then(function() {
+        console.log(req.body);
+        createCourse(req.body.name,req.body.course,req.body.dob,req.body.age)
+
     });
     res.send('Data received:\n' + JSON.stringify(req.body));
 });
