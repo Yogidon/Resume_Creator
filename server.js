6f6f5aae51e1e2218406cce0785be69e40297dc7
yogi.yogesh361 <yogi.yogesh361@gmail.com>
2018-08-15 23:49:51 +0530

    Show the Database.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 hbs.registerPartials('./views/partials');
+
 app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/',express.static('./public'));
@@ -14,41 +15,44 @@ const courseSchema = new mongoose.Schema({
     name : String,
     course : String,
     dob : { type: Date, default: Date.now},
-    age : Number
+    age : Number,
+    gender : String
 });
 
 const Course = mongoose.model('course', courseSchema);
 
-async function createCourse(name, courses, dob,age ){
+async function createCourse(name,courses,dob,age,gender ){
     const course = new Course({
         name : name ,
         course:courses,
         dob:dob,
-        age:age
+        age:age,
+        gender:gender
     });
-
     const result = await course.save();
     console.log(result);
 }
 
-app.post('/post-feedback', function (req, res) {
-    mongoose.connect('mongodb://localhost/admin')
+app.post('/submit', function (req, res) {
+    mongoose.connect('mongodb://localhost/Form_Data')
         .then(() => {
-            console.log('Connected to MongoDB')
-            createCourse(req.body.name,req.body.course,req.body.dob,req.body.age)
+            console.log('Connected to MongoDB');
+            createCourse(req.body.name,req.body.course,req.body.dob,req.body.age,req.body.gender)
         })
         .catch(err => console.error('Could not connect to mongodb ...', err));
     res.send('Data received:\n' + JSON.stringify(req.body));
 });
 
-app.get('/view-feedbacks',  function(req, res) {
-    dbConn.then(function(db) {
-        db.collection('feedbacks').find({}).toArray().then(function(feedbacks) {
-            res.status(200).json(feedbacks);
-        });
+app.get('/show',  function(req, res) {
+    mongoose.connect('mongodb://localhost/Form_Data')
+        .then(() => {
+            Course.find({},(err,found)=> {
+                res.send(found)
+            }).sort({name : 1})
+        })
     });
-});
 
 app.listen(PORT,() => {
    console.log(`The Server is listeing on port ${PORT}`)
 });
+
